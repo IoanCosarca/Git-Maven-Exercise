451d37ed00d040974d959b887ca8539139fd8a5d
ioan.cosarca <ionutcosarca1@gmail.com>
2023-07-14 12:57:37 +0300

    Added number operations

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,14 @@
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Main");
+        int a = 3;
+        int b = 5;
+        double c = 2.25;
+        double d = 4.17;
+
+        System.out.println(NumberUtils.compare(a, b));
+        System.out.println(NumberUtils.toInt("45"));
+        System.out.println(NumberUtils.max(c, d));
     }
 }
