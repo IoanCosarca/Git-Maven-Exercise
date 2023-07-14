3382bcfe5ae9189ff3c4a27f63cef8c4da8022db
ioan.cosarca <ionutcosarca1@gmail.com>
2023-07-14 12:55:57 +0300

    Added string operations

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,11 @@
+import org.apache.commons.lang3.StringUtils;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Main");
+        System.out.println("Hello World!");
+        System.out.println(StringUtils.capitalize("hello world!"));
+
+        System.out.println(StringUtils.abbreviate("Hello World", 10));
+        System.out.println(StringUtils.getCommonPrefix("Apple", "Application", "Appliance"));
     }
 }
